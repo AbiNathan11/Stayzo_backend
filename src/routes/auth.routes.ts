f89931e04480c370b03c14f8da3ab5592f4033a1
AbiNathan11 <abiramythirulinganathan@gmail.com>
2026-05-31 22:36:48 +0530

    backend

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { sendOtp, verifyOtp } from '../controllers/auth.controller';
+import { sendOtp, verifyOtp, updateProfile, getProfile } from '../controllers/auth.controller';
 
 const router = Router();
 
 router.post('/send-otp', sendOtp);
 router.post('/verify-otp', verifyOtp);
+router.put('/update-profile', updateProfile);
+router.get('/profile/:email', getProfile);
 
 export default router;
